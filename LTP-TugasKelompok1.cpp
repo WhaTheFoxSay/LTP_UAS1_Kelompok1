0277d95f55d8ac9ca096861c80605a75b79063cc
eFoxS <76924382+WhaTheFoxSay@users.noreply.github.com>
2023-01-31 00:02:50 +0700

    Update LTP-TugasKelompok1.cpp
    Update v.1.0.2

--- LTP-TugasKelompok1.cpp
+++ LTP-TugasKelompok1.cpp
@@ -13,172 +13,199 @@ class suhu
 
 public :
 
-	float c, k, f, r;
+    float c, k, f, r;
 
-      void celcius ()
+    void celcius ()
 
-      {
+    {
 
-	cout<<"PROGRAM C++ KONVERSI SUHU DARI CELCIUS\n";
-	cout<<"Masukan Suhu : ";
-	cin>>c;
+        cout<<"PROGRAM C++ KONVERSI SUHU DARI CELCIUS\n";
+        cout<<"Masukan Suhu : ";
+        cin>>c;
 
-	k = c+273.15;
-	f = (c*1.8)+32;
-	r = c*0.8;
+        k = c+273.15;
+        f = (c*1.8)+32;
+        r = c*0.8;
 
-	cout<<"\nHasil Celcius Ke Kelvin     : "<<k;
-	cout<<"\nHasil Celcius Ke Fahrenheit : "<<f;
-	cout<<"\nHasil Celcius Ke Reamur    : "<<r;
+        cout<<"\nHasil Celcius Ke Kelvin     : "<<k;
+        cout<<"\nHasil Celcius Ke Fahrenheit : "<<f;
+        cout<<"\nHasil Celcius Ke Reamur    : "<<r;
 
-            cout<<endl;
+        cout<<endl;
 
-      }
-
-
-      void kelvin ()
-
-      {
-
-	cout<<"PROGRAM C++ KONVERSI SUHU DARI KELVIN\n";
-	cout<<"Masukan Suhu : ";
-	cin>>k;
-
-	c = k-273.15;
-	f = k*1.8-459.67;
-	r = (k-273.15)*0.8;
-
-	cout<<"\nHasil Kelvin Ke Celcius     : "<<c;
-	cout<<"\nHasil Kelvin Ke Fahrenheit : "<<f;
-	cout<<"\nHasil Kelvin Ke Reamur    : "<<r;
-
-            cout<<endl;
-
-      }
+    }
 
 
-      void fahrenheit ()
+    void kelvin ()
 
-      {
+    {
 
-	cout<<"PROGRAM C++ KONVERSI SUHU DARI FAHRENHEIT\n";
-	cout<<"Masukan Suhu : ";
-	cin>>f;
+        cout<<"PROGRAM C++ KONVERSI SUHU DARI KELVIN\n";
+        cout<<"Masukan Suhu : ";
+        cin>>k;
 
-	c = (f-32)/1.8;
-	k = (f+459.67)/1.8;
-	r = (f-32)*0.44;
+        c = k-273.15;
+        f = k*1.8-459.67;
+        r = (k-273.15)*0.8;
 
-	cout<<"\nHasil Fahrenheit Ke Celcius     : "<<c;
-	cout<<"\nHasil Fahrenheit Ke Kelvin : "<<k;
-	cout<<"\nHasil Fahrenheit Ke Reamur    : "<<r;
+        cout<<"\nHasil Kelvin Ke Celcius     : "<<c;
+        cout<<"\nHasil Kelvin Ke Fahrenheit : "<<f;
+        cout<<"\nHasil Kelvin Ke Reamur    : "<<r;
 
-            cout<<endl;
+        cout<<endl;
 
-      }
-	  
-	        void reamur ()
+    }
 
-      {
 
-	cout<<"PROGRAM C++ KONVERSI SUHU DARI REAMUR\n";
-	cout<<"Masukan Suhu : ";
-	cin>>r;
+    void fahrenheit ()
 
-	c = r/0.8;
-	k = r/0.8 + 273.15;
-	f = r*2.25+32;
+    {
 
-	cout<<"\nHasil Reamur Ke Celcius     : "<<c;
-	cout<<"\nHasil Reamur Ke Kelvin : "<<k;
-	cout<<"\nHasil Reamur Ke Fahrenheit    : "<<f;
+        cout<<"PROGRAM C++ KONVERSI SUHU DARI FAHRENHEIT\n";
+        cout<<"Masukan Suhu : ";
+        cin>>f;
 
-            cout<<endl;
+        c = (f-32)/1.8;
+        k = (f+459.67)/1.8;
+        r = (f-32)*0.44;
 
-      }
+        cout<<"\nHasil Fahrenheit Ke Celcius     : "<<c;
+        cout<<"\nHasil Fahrenheit Ke Kelvin : "<<k;
+        cout<<"\nHasil Fahrenheit Ke Reamur    : "<<r;
+
+        cout<<endl;
+
+    }
+
+    void reamur ()
+
+    {
+
+        cout<<"PROGRAM C++ KONVERSI SUHU DARI REAMUR\n";
+        cout<<"Masukan Suhu : ";
+        cin>>r;
+
+        c = r/0.8;
+        k = r/0.8 + 273.15;
+        f = r*2.25+32;
+
+        cout<<"\nHasil Reamur Ke Celcius     : "<<c;
+        cout<<"\nHasil Reamur Ke Kelvin : "<<k;
+        cout<<"\nHasil Reamur Ke Fahrenheit    : "<<f;
+
+        cout<<endl;
+
+    }
 
 };
+
+class  about{
+
+public:
+
+    void kami()
+    {
+        cout << "----------------------------------------------------------------------------\t"<<endl;
+        cout << "Program ini dibuat oleh Kelompok 1 pada Mata Kuliah Logika Teknik Pemograman\t"<<endl;
+        cout << "============================================================================\t"<<endl;
+        cout << "                                Kelompok 1                                  \t"<<endl;
+        cout << " 1. Ferdinan Rizky Budiarto" << "(1002220005)\t"<<endl;
+        cout << " 2. Rinanto Pratama Adhi Prayoga" << "(10022200040)\t"<<endl;
+        cout << "============================================================================\t"<<endl;
+        cout << "                              Dosen Pengampu                                \t"<<endl;
+        cout << "Royan Habibie Sukarna, S.Kom., M.Kom.                                       \t"<<endl;
+        cout << "============================================================================\t"<<endl;
+    }
+};
+
 
 int main()
 
 {
 
-      int pilihan;
+    int pilihan;
 
-      atas :
+    atas :
+    about tentang;
+    suhu x;
 
-      suhu x;
+    cout<<"================================================================================"<<endl;
 
-      cout<<"================================================================================"<<endl;
+    cout<<"\t\t\t\t\t\t"<<endl;
 
-      cout<<"\t\t_ \t\t\t"<<endl;
+    cout<<"\t\tObject Oriented Programming ( OOP ) C++ \t\t\t"<<endl;
 
-      cout<<"\t\tObject Oriented Programming ( OOP ) C++ \t\t\t"<<endl;
+    cout<<"================================================================================"<<endl;
 
-      cout<<"================================================================================"<<endl;
+    cout<<" Menu Pilihan "<<endl;
 
-      cout<<" Menu Pilihan "<<endl;
+    cout<<" 1. KONVERSI SUHU DARI CELCIUS "<<endl;
 
-      cout<<" 1. KONVERSI SUHU DARI CELCIUS "<<endl;
+    cout<<" 2. KONVERSI SUHU DARI KELVIN "<<endl;
 
-      cout<<" 2. KONVERSI SUHU DARI KELVIN "<<endl;
+    cout<<" 3. KONVERSI SUHU DARI FAHRENHEIT "<<endl;
 
-      cout<<" 3. KONVERSI SUHU DARI FAHRENHEIT "<<endl;
-	  
-	  cout<<" 4. KONVERSI SUHU DARI REAMUR "<<endl;
+    cout<<" 4. KONVERSI SUHU DARI REAMUR "<<endl;
 
-      cout<<" 5. Exit Program "<<endl;
+    cout<<" 5. Tentang Pengembang "<<endl;
 
-      cout<<endl;
+    cout<<" 6. Exit Program "<<endl;
 
-      cout<<" Input Nomor Menu Pilihan = ";cin>>pilihan;
+    cout<<endl;
 
-      cout<<endl;
+    cout<<" Input Nomor Menu Pilihan = ";cin>>pilihan;
+
+    cout<<endl;
 
 
-      switch (pilihan)
+    switch (pilihan)
 
-      {
+    {
 
-      case 1 : x.celcius ();
-
-            goto atas;
-
-            break;
-
-      case 2 : x.kelvin ();
+        case 1 : x.celcius ();
 
             goto atas;
 
             break;
 
-      case 3 : x.fahrenheit ();
+        case 2 : x.kelvin ();
 
             goto atas;
 
             break;
-			
-      case 4 : x.reamur ();
+
+        case 3 : x.fahrenheit ();
 
             goto atas;
 
-            break;			
+            break;
 
-      case 5 : exit:
+        case 4 : x.reamur ();
 
+            goto atas;
+
+            break;
+
+        case 5 : tentang.kami ();
+
+            goto atas;
+
+            break;
+
+        case 6 : exit :
             cout<<" Anda Kembali ke Halaman Program "<<endl;
 
             break;
 
-      default: cout <<"Anda Salah Input Pilihan"<<endl;
+        default: cout << "Input tidak dikenali, Masukan dengan Benar|!!";
 
             goto atas;
 
 
-      }
+    }
 
-      system ("pause");
+    system ("pause");
 
-      return 0;
+    return 0;
 
 }
